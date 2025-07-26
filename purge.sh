96fc2ed2a2428d0ff9f70ef22dc484f97e496e3a
Alexis Hachemi <alexishachemi@gmail.com>
2025-07-27 00:26:21 +0200

    Update structure and file tracked

--- purge.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# dependency for the installer
-echo checking python...
-sudo apt install python3 -qq > /dev/null 2> /dev/null
-
-echo running installer
-python3 src/installer.py purge && echo done
